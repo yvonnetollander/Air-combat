1289896a83e133e82685210ca4bae3961b1f5167
Yvonne Tollander de Balsch <yvonne.tollander@gmail.com>
2019-11-15 14:13:03 +0200

    Comments

--- src/Plane.hpp
+++ src/Plane.hpp
@@ -2,6 +2,8 @@
 
 #include "Troop.hpp"
 
+/* Plane is a class that represents a troop. 
+   Plane takes parameters position, velocity, spritepath, rotation , status (dead or alive), health points and drag */
 class Plane : public Troop {
 public:
     Plane();
@@ -14,10 +16,11 @@ protected:
     float drag_;
 };
 
+/* PlayerPlane is user controllable plane and takes same parameters as Plane */
 class PlayerPlane : public Plane {
 public:
-    PlayerPlane(const sf::Vector2f& p, const std::string spritepath, const float r, const bool d, const unsigned hp, float drag);
-    
+    PlayerPlane(const sf::Vector2f& p, const std::string spritepath, const float r, const bool d, const unsigned hp, float drag); 
+    // Method to listen the key presses which control the plane
     void press_keys(Keys keys_pressed);
 private:
     void fire() {} 
