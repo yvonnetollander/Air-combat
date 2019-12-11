158a69a124553ad5b920c5d40bb6ff1f43e66f72
Johannes Haggren <haggrej1@aalto.fi>
2019-12-11 13:00:31 +0200

    Soldiers are now facing in the direction they're moving

--- src/Infantry.cpp
+++ src/Infantry.cpp
@@ -9,7 +9,7 @@ Infantry::Infantry()
     : Troop(), targetPos_(), idle_(0), wanderRadius_(0) { PickTarget(); }
 
 Infantry::Infantry(const sf::Vector2f& p, const std::string spritepath, const float r, const bool d, const unsigned hp, const float radius, const int min_x, const int max_x, unsigned ammo_left)
-    : Troop(p, sf::Vector2f(5.0f, 0.0f), spritepath, r, d, hp, ammo_left), targetPos_(), idle_(0), wanderRadius_(radius), min_x_(min_x), max_x_(max_x) { PickTarget(); }
+    : Troop(p, sf::Vector2f(50.0f, 0.0f), spritepath, r, d, hp, ammo_left), targetPos_(), idle_(0), wanderRadius_(radius), min_x_(min_x), max_x_(max_x) { PickTarget(); }
 
 Projectile* Infantry::Act(float dt, const sf::Vector2f& player_pos, const sf::Vector2f& player_velocity) {
     if (!dead_) {
@@ -43,9 +43,10 @@ Projectile* Infantry::Act(float dt, const sf::Vector2f& player_pos, const sf::Ve
 void Infantry::PickTarget() {
     float random = randFloat();
     // Infantry move on the x-axis only
-    int new_x = 2.0f * (random - 0.5f) * wanderRadius_;
+    int new_x = 20.0f * (random - 0.5f) * wanderRadius_;
     if (new_x < min_x_) { new_x = min_x_; }
     else if (new_x > max_x_) { new_x = max_x_; }
+    if ( (new_x < pos_.x && x_multiplier_ == 1) || (new_x > pos_.x && x_multiplier_ == -1) ) FlipX();
     targetPos_ = sf::Vector2f(new_x, 0);
 }
 
@@ -55,7 +56,7 @@ Projectile* Infantry::FireMachineGun(const sf::Vector2f& player_pos) {
         // Set the bullet go towards the player
         sf::Vector2f bullet_velocity = lengthen(player_pos, 1000);
         sf::Vector2f bullet_pos = pos_ + (normalize(velocity_) * 10.0f);
-        Projectile* projectiles = new Projectile(bullet_pos, bullet_velocity, ROOTDIR + "/res/bullet_small.png", 0.f, false, damage_radius, 50);
+        Projectile* projectiles = new Projectile(bullet_pos, bullet_velocity, ROOTDIR + "/res/bullet_small.png", 0.f, false, damage_radius, 10);
         machine_gun_cooldown_left_ = machine_gun_cooldown_;
         ammo_left_ -= 1;
         return projectiles;
--- src/MovingEntity.cpp
+++ src/MovingEntity.cpp
@@ -4,7 +4,7 @@ MovingEntity::MovingEntity()
     : GameEntity(), velocity_() { }
 
 MovingEntity::MovingEntity(const sf::Vector2f& p, const sf::Vector2f& v, const std::string spritepath, const float r, const bool d)
-    : GameEntity(p, spritepath, r, d) { velocity_ = v; }
+    : GameEntity(p, spritepath, r, d), x_multiplier_(1) { velocity_ = v; }
 
 Projectile* MovingEntity::Act(float dt) { 
     Move(dt);
@@ -17,4 +17,9 @@ void MovingEntity::Move(float dt) {
 
 const sf::Vector2f MovingEntity::getVelocity() const {
     return velocity_;
+}
+
+void MovingEntity::FlipX() {
+    x_multiplier_ = -x_multiplier_;
+    SetScale(sf::Vector2f(1.f * x_multiplier_, 1.f));
 }
