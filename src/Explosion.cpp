f144cb385dc169a0bdde08a7181f539ce3994a9a
Johannes Haggren <haggrej1@aalto.fi>
2019-12-12 18:51:24 +0200

    Added comments to explosion

--- src/Explosion.cpp
+++ src/Explosion.cpp
@@ -6,7 +6,7 @@ Explosion::Explosion()
     : MovingEntity(), phase_(0), timer_(0.f) {}
 
 Explosion::Explosion(sf::Vector2f position, float scale) 
-    : MovingEntity(position, sf::Vector2f(0.f, 0.f), ROOTDIR + "/res/explosion.png", 0, false), phase_(0), timer_(0.15) {
+    : MovingEntity(position, sf::Vector2f(0.f, 0.f), ROOTDIR + "/res/explosion.png", 0, false), phase_(0), timer_(0.1) {
         SetScale(sf::Vector2f(scale, scale));
         sf::IntRect rect = sf::IntRect(0, 0, 96, 96);
         sf::Vector2f pos = getPos();
@@ -21,7 +21,7 @@ Projectile* Explosion::Act(float dt) {
     if (timer_ < 0) {
         if (phase_ < 12) {
             phase_ += 1;
-            timer_ += 0.15;
+            timer_ += 0.1;
             sf::IntRect rect = sf::IntRect(phase_ * 96, 0, 96, 96);
             setTextureRect(rect);
         } else {
--- src/Explosion.hpp
+++ src/Explosion.hpp
@@ -3,15 +3,26 @@
 #include "MovingEntity.hpp"
 #include "Projectile.hpp"
 
-// Class for animated explosions
+/** Class Explosion represents explosions in the battlefield.
+    Creates an animated explosion with scaleable size.
+
+    Constructor parameters:
+        sf::Vector2f position       position of the entity
+        float scale                 size scaling for the explosion
+
+    Explosions are relative to time, and are updated with Act(dt)
+**/
 
 class Explosion : public MovingEntity {
 public:
     Explosion();
     Explosion(sf::Vector2f position, float scale = 1.f);
     ~Explosion();
+    // Change explosion phase as time passes
     virtual Projectile* Act(float dt);
 private:
+    // Counter for explosion phase
     int phase_;
+    // Timer for changing texture
     float timer_;
 };
