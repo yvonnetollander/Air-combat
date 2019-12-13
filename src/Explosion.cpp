c705cbd87b918a3f3a9f419ee373539490f2074e
Johannes Haggren <haggrej1@aalto.fi>
2019-12-13 10:27:55 +0200

    Added comments to explosion.cpp

--- src/Explosion.cpp
+++ src/Explosion.cpp
@@ -7,7 +7,9 @@ Explosion::Explosion()
 
 Explosion::Explosion(sf::Vector2f position, float scale) 
     : MovingEntity(position, sf::Vector2f(0.f, 0.f), ROOTDIR + "/res/explosion.png", 0, false), phase_(0), timer_(0.1) {
+        // Scale the explosion
         SetScale(sf::Vector2f(scale, scale));
+        // Cut rectangle texture and reposition it in the middle of the original one
         sf::IntRect rect = sf::IntRect(0, 0, 96, 96);
         sf::Vector2f pos = getPos();
         setPos(pos.x + scale * (getSize().x / 2 - getSize().y / 2), pos.y);
@@ -19,12 +21,14 @@ Explosion::~Explosion() {}
 Projectile* Explosion::Act(float dt) {
     timer_ -= dt;
     if (timer_ < 0) {
+        // Change the texture
         if (phase_ < 12) {
             phase_ += 1;
             timer_ += 0.1;
             sf::IntRect rect = sf::IntRect(phase_ * 96, 0, 96, 96);
             setTextureRect(rect);
         } else {
+        // Set the status to dead
             kill();
         }
     }
